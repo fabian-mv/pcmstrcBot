1be4271454bf1a061b6ab0abb738b2feb7903b1b
machetazo <fabianmontero1998@gmail.com>
2016-12-12 01:24:33 -0600

    deleted crap code
    ez4machetazo

--- pcmstrcBot.py
+++ pcmstrcBot.py
@@ -1,4 +1,5 @@
 import urllib.request
+import simplejson
 
 ##------------------------VARIABLES AND DECLARATIONS------------------------##
 
@@ -18,6 +19,7 @@ sendMessageCMD = "/sendmessage"
 
 #CHAT IDs
 pcmstrcID = "-1001095327132"
+
 caroRodriguezID = "8869779"
 chemaID = "275473930"
 julioID = "252442498"
@@ -42,33 +44,11 @@ def getUpdate():
     with urllib.request.urlopen(link) as response:
         update = response.read()
 
+
     print(update)
-
-
-"""
-    update = update[::-1]
-
-    updateLen = len(update)
-
-    for i in range(updateLen):
-        if update[i] == "d":
-            if update[i+1] == "i":
-                if update[i+2] == "_":
-                    if update[i+3] == "e":
-                        if update[i+4] == "t":
-                            update = update[i+4::-1]
-                            print(update)
-
-
-
-
-
-    print("error")
-
-"""
 
 ##------------------------EXECUTE------------------------##
 
-#sendMessage("testerino" , pcmstrcID)
+sendMessage("testerino" , pcmstrcID)
 
-getUpdate()
+#getUpdate()
